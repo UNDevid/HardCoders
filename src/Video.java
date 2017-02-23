43f2bc2f855585fb0c82a02e5ccd30edc3146e03
UNDevid <dvalentino94@gmail.com>
2017-02-23 19:28:17 +0100

    Update Video.java

---
+++ src/Video.java
@@ -0,0 +1,30 @@
+
+public class Video {
+	
+	private int id;
+	private int size;
+	
+	public Video(int id, int size) {
+		this.id = id;
+		this.size = size;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	@Override
+	public String toString() {
+		return "Video [id=" + id + ", size=" + size + "]";
+	}
+
+}
